d06c7d3533b097280ef7acc129a20d7a7a674283
+ <ronob7898@yahoo.com>
2024-07-14 22:33:58 +0300

    Fixes the main element & add newHeader victory msg

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const newMain = document.querySelector("main#main");
+newMain.remove();
+//creates a new h1
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Brian Kimutai is the champion';//Brian Kimutai is the champion
+
+document.body.appendChild(newHeader);//append newHeader to body
